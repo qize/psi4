095291900b4a5bad7ae7e1fde97d1cf8d4d8a3af
Qiao Zeng <18537488+qize@users.noreply.github.com>
2018-11-05 12:53:23 -0500

    x

--- test/dum.cpp
+++ test/dum.cpp
@@ -1,14 +1,22 @@
+#include "psi4/libpsi4util/PsiOutStream.h"
+#include "psi4/libmints/molecule.h"
 #include "psi4/libmints/extern.h"
 
-//namespace psi;
+//namespace psi{}
 
 int main(){
    double zz=1.2;
    double x=2.9;
    double y=-0.3;
    double z=9.2;
+   psi::Molecule mol;
+   mol.set_name("_abcdefg_");
+   printf("%s\n",mol.name().c_str());
    psi::ExternalPotential xx;
    xx.addCharge(zz,x, y,z);
-//   xx.print();
+   xx.addCharge(zz,x, y,z);
+   xx.addCharge(zz,x, y,z);
+   xx.addCharge(zz,x, y,z);
+   xx.print("wys");
    return 0;
  }
